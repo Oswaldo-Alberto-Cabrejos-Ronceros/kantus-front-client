f6954927319916acb53d6750841b39208cfe8771
Oswaldo <U22209742@utp.edu.pe>
2026-04-02 10:09:38 -0500

    feat: Added layouts

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <UHeader>
+    <template #left>
+      <NuxtLink to="/salon">
+        <div class="flex gap-4 items-center justify-center">
+          <img
+            src="/image/logo.jpg"
+            alt="Logo"
+            width="48"
+          >
+          <h2 class="text-primary text-2xl ">Salón</h2>
+        </div>
+
+      </NuxtLink>
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+      <UButton
+        icon="i-lucide-shopping-basket"
+        size="md"
+        color="primary"
+        variant="ghost"
+        to="/salon/pedido"
+      />
+    </template>
+  </UHeader>
+</template>
--- app/layouts/room.vue
+++ app/layouts/room.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <TheHeader />
+    <RoomTheHeader />
     <UMain>
       <slot />
     </UMain>
